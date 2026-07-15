3586ba9676cfb1d3853c2b0377c9b9f0ead28808
Night Agent <36kshania@gmail.com>
2026-07-15 16:11:08 +0300

    Update HexonLoader.lua

--- HexonLoader.lua
+++ HexonLoader.lua
@@ -1,1 +1,16 @@
+local GameID = game.PlaceId
+print(game.PlaceId)
 
+if GameID == 104522435597696 then
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NightAgentElite/Noxius/refs/heads/main/AHA.lua"))()
+
+elseif GameID == 7720943627 then
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NightAgentElite/Noxius/refs/heads/main/CCT.lua"))()
+
+else
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NightAgentElite/Noxius/refs/heads/main/Support.lua"))()
+
+end
